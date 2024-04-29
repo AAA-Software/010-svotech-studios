49fea99a4d65f444008ecc1772c7d098275e7e01
Braimah-Abiola <braimahabiola.official@gmail.com>
2024-04-29 22:49:41 +0000

    Fix structure

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -1,7 +1,13 @@
-const HomePage = () => {
-    return ( 
-        <div></div>
-     );
+import HeroSection from "@/components/sections/hero";
+import PreviousClientsSection from "@/components/sections/previous-clients";
+import SlidingProjects from "@/components/sections/slidding-portfolio";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+      <PreviousClientsSection />
+      <SlidingProjects />
+    </main>
+  );
 }
- 
-export default HomePage;
